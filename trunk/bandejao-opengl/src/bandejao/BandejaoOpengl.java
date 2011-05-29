4454a5e844573eee806dcb9f8a19fc7d758b2f39
matheusedsonramos <matheusedsonramos@edd39f85-4a77-99d1-9879-2f04eebbd9c8>
2011-05-29 22:04:15 +0000

    0.1

--- trunk/bandejao-opengl/src/bandejao/BandejaoOpengl.java
+++ trunk/bandejao-opengl/src/bandejao/BandejaoOpengl.java
@@ -1,6 +1,8 @@
 package bandejao;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import javax.media.opengl.GL;
 import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.GLCapabilities;
@@ -28,7 +30,7 @@ public class BandejaoOpengl extends GLJPanelInteractive{
         catraca = new Catraca(drawable);
 		extintor = new Extintor(drawable);
         maquinaCartao = new MaquinaCartao(drawable);
-		mesa = new Mesa(drawable);
+		criarMesas(drawable);
         gradeEntrada = new GradeEntrada(drawable);
         gradeMaquinaCartao = new GradeMaquinaCartao(drawable);
         gradeMaquinaCartao2 = new GradeMaquinaCartao2(drawable);
@@ -53,7 +55,7 @@ public class BandejaoOpengl extends GLJPanelInteractive{
 
         gl.glMatrixMode(GL.GL_MODELVIEW);
         
-        piso.desenha(drawable);     
+        //piso.desenha(drawable);
         gradeMaquinaCartao.desenha(drawable);
         gradeMaquinaCartao2.desenha(drawable);
 		extintor.desenha(drawable);
@@ -87,19 +89,57 @@ public class BandejaoOpengl extends GLJPanelInteractive{
     }
 	public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean deviceChanged) {
     }
+	/**
+	 * Cria as mesas no ArrayList
+	 * @param drawable
+	 */
+	private void criarMesas(GLAutoDrawable drawable){
+		mesas_ = new ArrayList();
+		Mesa mesa;
+		for(int i=0; i<10; i++){
+			mesa = new Mesa(drawable);
+			mesas_.add(mesa);
+		}
 
+	}
+	/**
+	 * Percorre o ArrayList e desenha (de perto dos ármarios para perto da cozinha) as mesas.
+	 *
+	 * @param drawable
+	 */
 	private void desenharMesas(GLAutoDrawable drawable){
 		GL gl = drawable.getGL();
+		Iterator it;	//Iterador
+		int i=0;		//Variável auxiliar que ajudará a transladar de volta ao local correto*/
+		int fileira=0;	//Número da fileira atual
+		Mesa mesa;		//Váriavel temporária que armazenará as mesas
+
+		/*Medidas referentes às mesas*/
 		final float ESPACO_ENTRE_MESAS = 3.5f;
-		int i=0;
+		final float ESPACO_ENTRE_FILEIRAS = 4f;
+		final float ESPACO_FILEIRA_PRINCIPAL = 5f;
+
+		//Cria iterador
+		it = mesas_.iterator();
 
 		/*Primeira Fileira*/
-		gl.glTranslatef(-10f,0f,0f);
-		for(i=0; i<10; i++){
-			gl.glTranslatef(0f, 0f, ESPACO_ENTRE_MESAS);
+		gl.glTranslatef(-10f,0f,15f);	//Mesa Inicial
+		for(i=0; it.hasNext() && i<4; i++){
+			gl.glTranslatef(0f, 0f, -ESPACO_ENTRE_MESAS);
+			mesa = (Mesa) it.next();
 			mesa.desenha(drawable);
 		}
-		gl.glTranslatef(+10f, 0f, -i*ESPACO_ENTRE_MESAS);
+		fileira++;
+		gl.glTranslatef(10f, 0f, i*ESPACO_ENTRE_MESAS-15);
+
+		/*Segunda Fileira*/
+		gl.glTranslatef(-10f-ESPACO_ENTRE_FILEIRAS,0f,15f);
+		for(i=0; it.hasNext() && i<4; i++){
+			gl.glTranslatef(0f, 0f, -ESPACO_ENTRE_MESAS);
+			mesa = (Mesa) it.next();
+			mesa.desenha(drawable);
+		}
+		gl.glTranslatef(10f+ESPACO_ENTRE_FILEIRAS, 0f, i*ESPACO_ENTRE_MESAS-15);
 	}
 
     private void lighting(GLAutoDrawable drawable) {
@@ -195,7 +235,7 @@ public class BandejaoOpengl extends GLJPanelInteractive{
     private static Catraca catraca;
 	private static Extintor extintor;
     private static MaquinaCartao maquinaCartao;
-	private static Mesa mesa;
+	private static ArrayList mesas_;
     private static GradeMaquinaCartao gradeMaquinaCartao;
     private static GradeMaquinaCartao2 gradeMaquinaCartao2;
     private static GradeEntrada gradeEntrada;
