70f9b52e37d31a134dca02a24ef0b4b1af2d4f64
matheusedsonramos <matheusedsonramos@edd39f85-4a77-99d1-9879-2f04eebbd9c8>
2011-05-29 22:04:42 +0000

    0.1

---
+++ trunk/bandejao-opengl/src/objetos/Mesa.java
@@ -0,0 +1,9 @@
+package objetos;
+
+import javax.media.opengl.GLAutoDrawable;
+
+public class Mesa extends ObjetoGenerico {
+	public Mesa(GLAutoDrawable drawable) {
+		super(drawable,"Mesa.obj");
+	}
+}
