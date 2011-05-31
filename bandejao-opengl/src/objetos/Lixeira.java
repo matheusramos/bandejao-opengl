0238c3d45d83625a6a2a5e7c5e213cd02118cb8d
matheusedsonramos <matheusedsonramos@edd39f85-4a77-99d1-9879-2f04eebbd9c8>
2011-05-31 17:34:22 +0000

    Lixeira adicionada

---
+++ bandejao-opengl/src/objetos/Lixeira.java
@@ -0,0 +1,9 @@
+package objetos;
+
+import javax.media.opengl.GLAutoDrawable;
+
+public class Lixeira extends ObjetoGenerico {
+	public Lixeira(GLAutoDrawable drawable) {
+		super(drawable,"Lixeira.obj");
+	}
+}
